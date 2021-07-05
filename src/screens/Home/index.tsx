9d193062e8c81e021fc349de8b9ed5389534d125
Giovanne Tarcitano <giovannetarcitano@gmail.com>
2021-07-04 23:45:26 -0300

    chore: add Header component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 
-import { Container, Title } from './styles';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { useTheme } from 'styled-components/native';
+
+import Logo from '../../assets/logo.svg';
+import { SafeArea } from '../../components/SafeArea';
+import { Container, Header, TotalCars, HeaderContent } from './styles';
 
 export function Home(): JSX.Element {
+  const { colors } = useTheme();
   return (
-    <Container>
-      <Title>Home</Title>
-    </Container>
+    <SafeArea style={{ backgroundColor: colors.header }} removePaddingBottom>
+      <Container>
+        <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+        <Header>
+          <HeaderContent>
+            <Logo width={RFValue(108)} height={RFValue(12)} />
+            <TotalCars>Total de 12 carros</TotalCars>
+          </HeaderContent>
+        </Header>
+      </Container>
+    </SafeArea>
   );
 }
