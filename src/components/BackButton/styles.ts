c69195ff06759b22275471185dc8147bd9f41065
Giovanne Tarcitano <giovannetarcitano@gmail.com>
2021-07-12 19:48:45 -0300

    fix: BackButton style

--- src/components/BackButton/styles.ts
+++ src/components/BackButton/styles.ts
@@ -2,10 +2,6 @@ import { BorderlessButton } from 'react-native-gesture-handler';
 import styled from 'styled-components/native';
 
 export const Container = styled(BorderlessButton)`
-  flex: 1;
-  background-color: ${({ theme }) => theme.colors.background_secondary};
-`;
-
-export const Header = styled.View`
-  flex: 1;
+  height: 20px;
+  width: 20px;
 `;
