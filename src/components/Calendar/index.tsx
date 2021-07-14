f1ffc0d7d2e7790aaf611a21716ce58457754b0a
Giovanne Tarcitano <giovannetarcitano@gmail.com>
2021-07-13 21:53:52 -0300

    chore: add Calendar component

---
+++ src/components/Calendar/index.tsx
@@ -0,0 +1,72 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+
+import { Feather } from '@expo/vector-icons';
+import { Calendar as CustomCalendar, LocaleConfig } from 'react-native-calendars';
+import { useTheme } from 'styled-components/native';
+
+LocaleConfig.locales['pt-br'] = {
+  monthNames: [
+    'Janeiro',
+    'Fevereiro',
+    'Março',
+    'Abril',
+    'Maio',
+    'Junho',
+    'Julho',
+    'Agosto',
+    'Setembro',
+    'Outubro',
+    'Novembro',
+    'Dezembro',
+  ],
+  monthNamesShort: [
+    'Jan',
+    'Fev',
+    'Mar',
+    'Abr',
+    'Mai',
+    'Jun',
+    'Jul',
+    'Ago',
+    'Set',
+    'Out',
+    'Nov',
+    'Dez',
+  ],
+  dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+  dayNamesShort: ['DOM', 'SEG', 'TER', 'QUA', 'QUI', 'SEX', 'SÁB'],
+  today: 'Hoje',
+};
+LocaleConfig.defaultLocale = 'pt-br';
+
+export function Calendar(): JSX.Element {
+  const { colors, fonts } = useTheme();
+  return (
+    <CustomCalendar
+      renderArrow={direction => (
+        <Feather name={`chevron-${direction}`} size={24} color={colors.text} />
+      )}
+      headerStyle={{
+        backgroundColor: colors.background_secondary,
+        borderBottomWidth: 0.5,
+        borderBottomColor: colors.text_detail,
+        paddingBottom: 8,
+        marginBottom: 8,
+      }}
+      theme={{
+        textDayFontFamily: fonts.primary_400,
+        textDayHeaderFontFamily: fonts.primary_500,
+        textDayHeaderFontSize: 10,
+        textMonthFontSize: 20,
+        textMonthFontFamily: fonts.secondary_600,
+        monthTextColor: colors.title,
+        arrowStyle: {
+          marginHorizontal: -15,
+        },
+      }}
+      firstDay={1}
+      minDate={new Date()}
+    />
+  );
+}
